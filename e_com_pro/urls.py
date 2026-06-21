3514595cc0260a0fbb59db12e59f3a418e11e6d3
yakash4658r <yakashelavarasan@outlook.com>
2026-06-21 15:29:26 +0530

    Fix NameError: restore missing static and serve imports

--- e_com_pro/urls.py
+++ e_com_pro/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),

79eb5318661daadbe3035cf2eff632cad7abf3bc
yakash4658r <yakashelavarasan@outlook.com>
2026-06-27 20:50:19 +0530

    Enable Google Login via settings APP config

--- e_com_pro/settings.py
+++ e_com_pro/settings.py
@@ -52,6 +52,11 @@ SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': ['profile', 'email'],
         'AUTH_PARAMS': {'access_type': 'online'},
+        'APP': {
+            'client_id': os.environ.get('GOOGLE_CLIENT_ID', ''),
+            'secret': os.environ.get('GOOGLE_CLIENT_SECRET', ''),
+            'key': ''
+        }
     }
 }
 

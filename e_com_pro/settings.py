9373305ae522ea0db7bc818d15582843d428a8da
yakash4658r <yakashelavarasan@outlook.com>
2026-06-27 20:48:02 +0530

    Fix media root for production and prepare for live deployment

--- e_com_pro/settings.py
+++ e_com_pro/settings.py
@@ -130,10 +130,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 
-if os.environ.get('production', False):
-    MEDIA_ROOT = "/app/media"
-else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
